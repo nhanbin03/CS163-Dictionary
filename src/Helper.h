b72c542a703e2163cd770c63c12c65f5a0eb4f84
Truong Chi Nhan <nhanbin03@gmail.com>
2023-07-01 19:42:54 +0700

    Add helper functions

---
+++ src/Helper.h
@@ -0,0 +1,11 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <string>
+
+std::string convertToNonAccentVN(std::string str);
+
+// returns a string which contains only 'a' to 'z'
+std::string normalize(std::string str);
+
+#endif // HELPER_H
