419bbf7c24b6d782ec563a1316b015dde4c920c6
Truong Chi Nhan <nhanbin03@gmail.com>
2023-07-03 11:09:15 +0700

    Change description comment of normalize()

--- src/Helper.h
+++ src/Helper.h
@@ -5,7 +5,7 @@
 
 std::string convertToNonAccentVN(std::string str);
 
-// returns a string which contains only 'a' to 'z'
+// returns a string which convert uppercase letters to lowercase letters
 std::string normalize(std::string str);
 
 #endif // HELPER_H
