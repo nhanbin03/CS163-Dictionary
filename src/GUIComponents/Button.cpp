a2d7a82a48007e75162d64094d36ae72ff754392
Truong Chi Nhan <nhanbin03@gmail.com>
2023-07-05 21:58:43 +0700

    Fix bugs caused by copy-pasting from old proj

--- src/GUIComponents/Button.cpp
+++ src/GUIComponents/Button.cpp
@@ -1,6 +1,6 @@
 #include "Button.h"
 
-#include "../Helper.h"
+#include "GUIComponentHelper.h"
 
 #include <iostream>
 
---
+++ src/GUIComponents/GUIComponentHelper.h
@@ -0,0 +1,8 @@
+#ifndef GUICOMPONENTS_GUICOMPONENTHELPER_H
+#define GUICOMPONENTS_GUICOMPONENTHELPER_H
+
+#include "raylib.h"
+
+void scaleTexture(Texture2D &source, Vector2 newSize);
+
+#endif // GUICOMPONENTS_GUICOMPONENTHELPER_H
