cf4a26c756bdc20c19eec8176e9165273b71d483
Truong Chi Nhan <nhanbin03@gmail.com>
2023-07-13 20:50:04 +0700

    Add raylib Vector2 math operator overload

--- src/GUIComponents/GUIComponentHelper.cpp
+++ src/GUIComponents/GUIComponentHelper.cpp
@@ -1,6 +1,22 @@
 #include "GUIComponentHelper.h"
 
-void scaleTexture(Texture2D& source, Vector2 newSize) {
+Vector2 operator+ (const Vector2 &a, const Vector2 &b) {
+    return {a.x + b.x, a.y + b.y};
+}
+
+Vector2 operator- (const Vector2 &a, const Vector2 &b) {
+    return {a.x - b.x, a.y - b.y};
+}
+
+Vector2 operator* (const Vector2 &a, float x) {
+    return {a.x * x, a.y * x};
+}
+
+Vector2 operator/ (const Vector2 &a, float x) {
+    return {a.x / x, a.y / x};
+}
+
+void scaleTexture(Texture2D &source, Vector2 newSize) {
     Image img = LoadImageFromTexture(source);
     ImageResize(&img, newSize.x, newSize.y);
     source = LoadTextureFromImage(img);
