2feb3146b54fbcb25026d3dcbf80a97849069740
TaiNangTre2k4 <ndt280504@gmail.com>
2023-07-18 22:20:26 +0700

    Get Word Quiz

--- src/Core/Core.cpp
+++ src/Core/Core.cpp
@@ -74,3 +74,33 @@ Core::Word* Core::getRandomWord(){
     int randomIndex = dist(gen);
     return mWordCollection[randomIndex];
 }
+
+std::pair<Core::Word*, std::array<Core::Definition*, 5>> Core::getWordQuiz(){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    //Create array of choices. Choice[0] is the answer.
+    Definition *choices[5];
+    //Get the random definition.
+    Word* question = getRandomWord();
+    std::uniform_int_distribution<> dist(0, question->defs.size() - 1);
+    int randomIndex = dist(gen);
+    choices[0] = question->defs[randomIndex];
+    //Get the other 4 random definitions. One of them is the answer.
+    std::uniform_int_distribution<> dist1(1,4);
+    randomIndex = dist1(gen);
+    choices[randomIndex] = question->defs[randomIndex];
+    for(int i = 1; i < 5; i++){
+        if(i != randomIndex){
+            Word* randomWord = getRandomWord();
+            while (randomWord == question) {
+                randomWord = getRandomWord();
+            }
+            std::uniform_int_distribution<> dist2(0, randomWord->defs.size() - 1);
+            int randomIndex2 = dist2(gen);
+            choices[i] = randomWord->defs[randomIndex2];
+        }
+    }
+    return std::make_pair(question, 
+    std::array<Definition*, 5>{choices[0], choices[1], choices[2], choices[3], choices[4]});
+}
