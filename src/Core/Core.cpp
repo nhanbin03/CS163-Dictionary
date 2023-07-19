317d95369958462a3d038f8b14a7181147b7106e
Truong Chi Nhan <nhanbin03@gmail.com>
2023-07-19 22:55:19 +0700

    Fix styling

--- src/Core/Core.cpp
+++ src/Core/Core.cpp
@@ -63,11 +63,11 @@ void Core::saveToFile() {
     */
 }
 
-std::vector<Core::Word*> Core::searchKeyword(const std::string &inputString){
+std::vector<Core::Word*> Core::searchKeyword(const std::string& inputString) {
     return mWordSet.getPrefixMatches(normalize(inputString));
 }
 
-Core::Word* Core::getRandomWord(){
+Core::Word* Core::getRandomWord() {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dist(0, mWordCollection.size() - 1);
@@ -75,37 +75,40 @@ Core::Word* Core::getRandomWord(){
     return mWordCollection[randomIndex];
 }
 
-std::pair<Core::Word*, std::array<Core::Definition*, 5>> Core::getWordQuiz(){
+std::pair<Core::Word*, std::array<Core::Definition*, 5>> Core::getWordQuiz() {
     std::random_device rd;
     std::mt19937 gen(rd());
 
-    //Create array of choices. Choice[0] is the answer.
-    Definition *choices[5];
-    //Get the random definition.
+    // Create array of choices. Choice[0] is the answer.
+    Definition* choices[5];
+    // Get the random definition.
     Word* question = getRandomWord();
     std::uniform_int_distribution<> dist(0, question->defs.size() - 1);
     int randomIndex = dist(gen);
     choices[0] = question->defs[randomIndex];
-    //Get the other 4 random definitions. One of them is the answer.
-    std::uniform_int_distribution<> dist1(1,4);
+    // Get the other 4 random definitions. One of them is the answer.
+    std::uniform_int_distribution<> dist1(1, 4);
     randomIndex = dist1(gen);
     choices[randomIndex] = question->defs[randomIndex];
-    for(int i = 1; i < 5; i++){
-        if(i != randomIndex){
+    for (int i = 1; i < 5; i++) {
+        if (i != randomIndex) {
             Word* randomWord = getRandomWord();
             while (randomWord == question) {
                 randomWord = getRandomWord();
             }
-            std::uniform_int_distribution<> dist2(0, randomWord->defs.size() - 1);
+            std::uniform_int_distribution<> dist2(0,
+                                                  randomWord->defs.size() - 1);
             int randomIndex2 = dist2(gen);
             choices[i] = randomWord->defs[randomIndex2];
         }
     }
-    return std::make_pair(question, 
-    std::array<Definition*, 5>{choices[0], choices[1], choices[2], choices[3], choices[4]});
+    return std::make_pair(
+        question, std::array<Definition*, 5>{choices[0], choices[1], choices[2],
+                                             choices[3], choices[4]});
 }
 
-std::pair<Core::Definition *, std::array<Core::Word *, 5>> Core::getDefinitionQuiz(){
+std::pair<Core::Definition*, std::array<Core::Word*, 5>>
+Core::getDefinitionQuiz() {
     // Get a definition.
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -116,11 +119,11 @@ std::pair<Core::Definition *, std::array<Core::Word *, 5>> Core::getDefinitionQu
     Word* choices[5];
     choices[0] = question->word;
     // Get the other 4 random words. One of them is the answer.
-    std::uniform_int_distribution<> dist1(1,4);
+    std::uniform_int_distribution<> dist1(1, 4);
     randomIndex = dist1(gen);
     choices[randomIndex] = question->word;
-    for(int i = 1; i < 5; i++){
-        if(i != randomIndex){
+    for (int i = 1; i < 5; i++) {
+        if (i != randomIndex) {
             Word* randomWord = getRandomWord();
             while (randomWord == question->word) {
                 randomWord = getRandomWord();
@@ -128,14 +131,15 @@ std::pair<Core::Definition *, std::array<Core::Word *, 5>> Core::getDefinitionQu
             choices[i] = randomWord;
         }
     }
-    return std::make_pair(question, 
-    std::array<Word*, 5>{choices[0], choices[1], choices[2], choices[3], choices[4]});
+    return std::make_pair(
+        question, std::array<Word*, 5>{choices[0], choices[1], choices[2],
+                                       choices[3], choices[4]});
 }
 
-std::vector< Core::Word *> Core::getFavoriteList(){
+std::vector<Core::Word*> Core::getFavoriteList() {
     std::vector<Word*> favoriteList;
-    for(Word* word : mWordCollection){
-        if(word->IsFavorite){
+    for (Word* word : mWordCollection) {
+        if (word->IsFavorite) {
             favoriteList.push_back(word);
         }
     }
