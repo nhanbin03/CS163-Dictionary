cf72dcd48e6c73feaaf17bc4c24d447f3a56c947
nguyen2192004 <phamgianguyen.qn@gmail.com>
2023-08-11 19:07:16 +0700

    sdas
    asdas

---
+++ src/Core/Dictionary.cpp
@@ -0,0 +1,35 @@
+#include "Dictionary.h"
+
+Core Dictionary::CORE[] = {
+    Core(
+        "engeng",
+        " !\"#$%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+        "abcdefghijklmnopqrstuvwxyz"),
+    Core(
+        "engvie",
+        " !\"#$%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+        "abcdefghijklmnopqrstuvwxyz"),
+    Core(
+        "vieeng",
+        " !\"#$%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+        "abcdefghijklmnopqrstuvwxyz"),
+    Core(
+        "slang",
+        " !\"#$%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+        "abcdefghijklmnopqrstuvwxyz"),
+    Core(
+        "emoji",
+        " !\"#$%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+        "abcdefghijklmnopqrstuvwxyz")
+};
+
+Dictionary& Dictionary::getInstance() {
+    static Dictionary instance;
+    return instance;
+}
+
+Dictionary::Dictionary() {
+}
+
+Dictionary::~Dictionary() {
+}
