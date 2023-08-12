cd3bbcc58ed6e655b6f8f6a4f825e25701fcad15
Dtaz125 <93046992+Dtaz125@users.noreply.github.com>
2023-08-12 17:24:40 +0700

    refactor Core.h

--- src/Core/Core.h
+++ src/Core/Core.h
@@ -18,17 +18,25 @@ public:
     struct Word;
 
     struct Definition {
+        std::string orgStr;
         std::string str;
         Word *word;
 
+        int rating;
+
         Definition(const std::string &str);
+
+        bool isDeleted();
     };
 
     struct Word {
+        std::string orgStr;
         std::string str;
         std::vector<Definition *> defs;
-        bool IsFavorite;
+        bool IsFavorite{false};
         Word(const std::string &str);
+
+        bool isDeleted();
     };
 
     struct DefWord {
@@ -39,31 +47,31 @@ public:
     };
 
 public:
-    Core(const std::string &specifier, const std::string &charSet);
+    Core(const std::string &specifier, const std::string &wordCharSet, const std::string defCharSet); 
     ~Core();
 
     std::vector<Word *> searchKeyword(const std::string &inputString);
-    std::vector<Word *> searchDefinition(const std::string &inputString);
+    std::vector<Word *> searchDefinition(const std::string &inputString); 
 
-    void updateHistory(Word *word);
-    std::vector<Word *> getHistory();
+    void updateHistory(Word *word); 
+    std::vector<Word *> getHistory(); 
 
     Word *addWord(std::string wordString);
-    Definition *addDefinition(std::string defString, Word *word);
-    void editDefinition(Definition *def, const std::string &newDef);
-    void removeWord(Word *word); // Caution!! Remember to delete its definitions
+    Definition *addDefinition(std::string defString, Word *word); 
+    void editDefinition(Definition *def, const std::string &newDef); 
+    void removeWord(Word *word); // Caution!! Remember to delete its definitions 
                                  // AND the word in history
 
-    Word *getRandomWord();
+    Word *getRandomWord(); 
 
-    void addFavorite(Word *word);
-    void removeFavorite(Word *word);
-    bool isFavorite(Word *word);
-    std::vector<Word *> getFavoriteList();
+    void addFavorite(Word *word); 
+    void removeFavorite(Word *word); 
+    bool isFavorite(Word *word); 
+    std::vector<Word *> getFavoriteList(); 
 
     // pair::first is the question, pair::second [1..4] are choices, [0] is the
     // answer
-    std::pair<Word *, std::array<Definition *, 5>> getWordQuiz();
+    std::pair<Word *, std::array<Definition *, 5>> getWordQuiz(); 
     std::pair<Definition *, std::array<Word *, 5>> getDefinitionQuiz();
   
     void resetDefault();
@@ -77,6 +85,7 @@ private:
 
     std::vector<Word *> mWordCollection;
     std::vector<Definition *> mDefCollection;
+    std::vector<DefWord *> mDefWordCollection;
 
     std::vector<Word *> mHistory;
 
@@ -87,10 +96,12 @@ private:
     void loadDataFromSpecifier(const std::string& mdataspecifier, std::vector<std::string>& words);
     void loadDataFromHistory(const std::string& mdataspecifier2 ) ;
     std::string extractSecondWord(const std::string& input);
-void  loadWordLocal(const std::string& mdataspecifier3) ;
-   void loadDefWordLocal(const std::string& mdataspecifier4);
-   std::string extractWord1(const std::string& input) ;
-   std::string extractWord2(const std::string& input);
+    void loadWordLocal(const std::string& mdataspecifier3);
+    std::string extractWord1(const std::string& input) ;
+    std::string extractWord2(const std::string& input);
+
+    void equivalentFilter1(std::vector<Definition *>& defResults, const std::string &inputString);
+    void equivalentFilter2(std::vector<Definition *> &defResults, const std::string &inputString);
 };
 
 #endif // CORE_CORE_H
