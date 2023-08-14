2247fde52f2616cba2b653acd10f8a937f99b621
TaiNangTre2k4 <ndt280504@gmail.com>
2023-08-14 18:05:16 +0700

    update helper.h

--- src/Helper.h
+++ src/Helper.h
@@ -2,10 +2,13 @@
 #define HELPER_H
 
 #include <string>
+#include <vector>
 
 std::string convertToNonAccentVN(std::string str);
 
 // returns a string which convert uppercase letters to lowercase letters
 std::string normalize(std::string str);
+
+std::vector<std::string> split(const std::string& str, char delimiter);
 
 #endif // HELPER_H
